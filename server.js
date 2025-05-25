8f40dfa63e1fece456ce418af4c26098be4b5a4d
alex soto <alejandro.soto@harness.io>
2025-05-25 16:00:58 -0400

    adding CORS support

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 let apiKeys = {};
